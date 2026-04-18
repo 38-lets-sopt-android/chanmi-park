9a9d8b14066066e7216ca68e786d45bffb273d89
너의이름 <qkrcksal1052@gmail.com>
2026-04-19 01:15:38 +0900

    [feat/#4] 뷰모델 구현

--- app/src/main/java/com/example/letssopt/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/letssopt/presentation/login/LoginActivity.kt
@@ -34,8 +34,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
